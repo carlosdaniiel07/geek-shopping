e030988105f82ee54c57809a8142ef637307c2ec
Carlos Daniel Martins de Almeida <hdn.carlos@voeazul.com.br>
2023-01-07 19:00:06 -0300

    feat: create project

---
+++ GeekShopping.Product.Api/Models/Context/MySqlContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductEntity = GeekShopping.Product.Api.Models.Entities.Product;
+
+namespace GeekShopping.Product.Api.Models.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+
+        public MySqlContext(DbContextOptions<MySqlContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
---
+++ GeekShopping.Product.Api/Models/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.Product.Api.Models.Entities
+{
+    [Table("product")]
+    public class Product : BaseEntity
+    {
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
+
+        [Column("description ")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Column("category_name")]
+        [Required]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string ImageUrl { get; set; }
+    }
+}
