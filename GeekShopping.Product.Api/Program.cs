a4c1bae04e89abb02c36840d14a65f88ed9e4f56
Carlos Daniel Martins de Almeida <hdn.carlos@voeazul.com.br>
2023-01-10 21:57:18 -0300

    feat: add dependencies to DI

--- GeekShopping.Product.Api/Program.cs
+++ GeekShopping.Product.Api/Program.cs
@@ -1,4 +1,6 @@
+using GeekShopping.Product.Api.Config;
 using GeekShopping.Product.Api.Models.Context;
+using GeekShopping.Product.Api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,11 @@ builder.Services.AddDbContext<MySqlContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
+var mapper = MappingConfig.RegisterMappings().CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
