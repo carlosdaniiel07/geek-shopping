5b1bcb20a8ca1abc2952004fe5a1b5c1de9b2a2b
Carlos Daniel Martins de Almeida <hdn.carlos@voeazul.com.br>
2023-01-10 21:56:57 -0300

    feat: create repository layer

---
+++ GeekShopping.Product.Api/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductEntity = GeekShopping.Product.Api.Models.Entities.Product;
+
+namespace GeekShopping.Product.Api.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductEntity>> GetAllAsync();
+        Task<ProductEntity> GetByIdAsync(Guid id);
+        Task SaveAsync(ProductEntity product);
+        Task UpdateAsync(ProductEntity product);
+        Task DeleteAsync(ProductEntity product);
+    }
+}
---
+++ GeekShopping.Product.Api/Repository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using GeekShopping.Product.Api.Models.Context;
+using Microsoft.EntityFrameworkCore;
+using ProductEntity = GeekShopping.Product.Api.Models.Entities.Product;
+
+namespace GeekShopping.Product.Api.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MySqlContext _context;
+        private readonly DbSet<ProductEntity> _dbSet;
+
+        public ProductRepository(MySqlContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<ProductEntity>();
+        }
+
+        public async Task<IEnumerable<ProductEntity>> GetAllAsync()
+        {
+            return await _dbSet
+                .AsNoTracking()
+                .OrderBy(product => product.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<ProductEntity> GetByIdAsync(Guid id)
+        {
+            return await _dbSet
+                .SingleOrDefaultAsync(product => product.Id == id);
+        }
+
+        public async Task SaveAsync(ProductEntity product)
+        {
+            await _dbSet.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(ProductEntity product)
+        {
+            _dbSet.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ProductEntity product)
+        {
+            _dbSet.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
