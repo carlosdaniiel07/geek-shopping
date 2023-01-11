5a0ea5f085a14a887bfa5465ef007c37f86577d1
Carlos Daniel Martins de Almeida <hdn.carlos@voeazul.com.br>
2023-01-10 21:56:44 -0300

    feat: setup AutoMapper

---
+++ GeekShopping.Product.Api/Config/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GeekShopping.Product.Api.Data.ValueObjects;
+using ProductEntity = GeekShopping.Product.Api.Models.Entities.Product;
+
+namespace GeekShopping.Product.Api.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductEntity, ProductVO>()
+                    .ReverseMap();
+            });
+        }
+    }
+}
