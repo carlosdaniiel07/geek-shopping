d58a491af39379dab76fdae3699f73d66efdd639
Carlos Daniel Martins de Almeida <carlosd.almeida@outlook.com>
2023-02-27 21:00:52 -0300

    feat: validate products while creating a new order on Order API

--- GeekShopping.Order.Api/Services/OrderService.cs
+++ GeekShopping.Order.Api/Services/OrderService.cs
@@ -12,13 +12,15 @@ namespace GeekShopping.Order.Api.Services
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IUserService _userService;
+        private readonly IProductService _productService;
         private readonly IMapper _mapper;
         private readonly IMessageBus _messageBus;
 
-        public OrderService(IOrderRepository orderRepository, IUserService userService, IMapper mapper, IMessageBus messageBus)
+        public OrderService(IOrderRepository orderRepository, IUserService userService, IProductService productService, IMapper mapper, IMessageBus messageBus)
         {
             _orderRepository = orderRepository;
             _userService = userService;
+            _productService = productService;
             _mapper = mapper;
             _messageBus = messageBus;
         }
@@ -32,10 +34,25 @@ namespace GeekShopping.Order.Api.Services
 
             var order = _mapper.Map<OrderEntity>(addOrderDto);
 
+            order.UserId = _userService.GetLoggedUserId();
+
             if (order.Total <= 0)
                 return new AddOrderResponseDto("O valor do pedido não pode ser 0 (zero)");
 
-            order.UserId = _userService.GetLoggedUserId();
+            var products = await _productService.GetAllByIdAsync(order.Items.Select(orderItem => orderItem.ProductId));
+
+            foreach (var orderItem in order.Items)
+            {
+                var product = products.FirstOrDefault(p => p.Id == orderItem.ProductId.ToString());
+
+                if (product == null)
+                    return new AddOrderResponseDto($"O produto {orderItem.Name} não foi encontrado");
+
+                var hasDifferentPrice = (decimal)product.Price != orderItem.Price;
+
+                if (hasDifferentPrice)
+                    return new AddOrderResponseDto($"O preço do produto {orderItem.Name} foi alterado");
+            }
 
             await _orderRepository.SaveAsync(order);
             await _messageBus.PublishAsync(ExchangesConfig.OrderCreated, _mapper.Map<OrderCreatedEvent>(order));
